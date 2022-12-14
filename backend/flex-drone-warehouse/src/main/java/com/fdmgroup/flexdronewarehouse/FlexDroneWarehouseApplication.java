56061f64b766a4a3f8e764deb31f08348fb94a05
Chris <christangsdy@gmail.com>
2022-12-15 10:39:50 +1100

    set up springboot

---
+++ backend/flex-drone-warehouse/src/main/java/com/fdmgroup/flexdronewarehouse/FlexDroneWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.flexdronewarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexDroneWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexDroneWarehouseApplication.class, args);
+	}
+
+}
