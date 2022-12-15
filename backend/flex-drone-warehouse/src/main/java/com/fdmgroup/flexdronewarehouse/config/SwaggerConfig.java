e9ddcf930578f2c0d933fff144bc13d14a92cdee
Chris <christangsdy@gmail.com>
2022-12-15 14:27:34 +1100

    finish config swagger

---
+++ backend/flex-drone-warehouse/src/main/java/com/fdmgroup/flexdronewarehouse/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.fdmgroup.flexdronewarehouse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * This class setUp the configuration of swagger.
+ * swagger is used to document all api automatically.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    /**
+     * set up swagger
+     * @return a Docket
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fdmgroup.flexdronewarehouse"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    /**
+     * set up mata data about api info
+     * @return apiInfo that will be displayed in api page
+     */
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Flex-drone warehouse - API Documentation")
+                .version("1.1.0")
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+
+    /**
+     * change path to a html page for displaying api
+     * @param registry api registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+
+    }
+
+}
