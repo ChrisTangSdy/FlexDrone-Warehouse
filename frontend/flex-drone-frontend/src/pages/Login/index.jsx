994e25614feb7a3186ba4d72281081d7e3cf8fc1
Majuan Su <sm@Majuans-MacBook-Pro.local>
2022-12-23 08:08:41 +1100

    stylise profile page and add form validation

--- frontend/flex-drone-frontend/src/pages/Login/index.jsx
+++ frontend/flex-drone-frontend/src/pages/Login/index.jsx
@@ -1,28 +1,66 @@
 import React, { useState } from "react";
 import { apis } from "../../utils/apis.js";
+import { FloatingLabel, Form, Button } from "react-bootstrap";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  // const [showError, setShowError] = useState(false);
+  const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
 
-  const onLoginSubmit = (e) => {
-    let loginInfo = { username, password };
-    apis
-      .logIn(loginInfo)
-      .then((res) => {
-        // save JWT into sessionStorage
-        sessionStorage.setItem("token", res.data);
-        // redirect the page
-        document.location.href = "/ViewUser";
-      })
-      .catch((err) => {
-        // if status code is 400/401/404
-        if (err.response.status.toString().startsWith("4"))
-          return alert("Incorrect username or password");
-        // if other status code
-        alert(err.response.data.message);
+  const setField = (field, value) => {
+    // add or update value for the field to the form
+    setForm({ ...form, [field]: value });
+
+    // remove the existing error for the field
+    if (!!errors[field]) {
+      setErrors({
+        ...errors,
+        [field]: null,
       });
+    }
+  };
+
+  // validate form and customize error messages
+  const validateForm = () => {
+    const { username, password } = form;
+    const newErrors = {};
+
+    if (!username || username === "")
+      newErrors.username = "Please enter your username";
+    if (!password || password === "")
+      newErrors.password = "Please enter your password";
+
+    return newErrors;
+  };
+
+  // handle login form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("login form submitted");
+
+    // validate form
+    const formErrors = validateForm();
+
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      // if no error, make API request
+      apis
+        .logIn(form)
+        .then((res) => {
+          // save JWT into sessionStorage
+          sessionStorage.setItem("token", res.data);
+          // redirect the page
+          document.location.href = "/ViewUser";
+        })
+        .catch((err) => {
+          // if status code is 400/401/404
+          if (err.response.status.toString().startsWith("4"))
+            return alert("Incorrect username or password");
+
+          // if other status code
+          alert(err.response.data.message);
+        });
+    }
   };
 
   return (
@@ -42,51 +80,50 @@ const Login = () => {
                 </div>
                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                   <div className="card-body p-4 p-lg-5 text-black">
-                    <form novalidate="">
-                      <div className="d-flex align-items-center mb-4 pb-1">
-                        <span className="h1 fw-bold mb-0">Sign In</span>
-                      </div>
+                    <Form onSubmit={handleSubmit}>
+                      <h1 className="fw-bold mb-4">Sign In</h1>
+                      <Form.Group controlId="username">
+                        <FloatingLabel label="Username" className="mb-4">
+                          <Form.Control
+                            type="text"
+                            value={form.username}
+                            onChange={(e) =>
+                              setField("username", e.target.value)
+                            }
+                            isInvalid={!!errors.username}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.username}
+                          </Form.Control.Feedback>
+                        </FloatingLabel>
+                      </Form.Group>
 
-                      <div className="form-floating mb-4">
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="floatingInput"
-                          placeholder="username123"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                          required
-                        />
-                        <label htmlFor="floatingInput">Username</label>
-                        <div class="invalid-feedback">
-                          Please choose a username.
+                      <Form.Group controlId="password">
+                        <FloatingLabel label="Password" className="mb-4">
+                          <Form.Control
+                            type="password"
+                            value={form.password}
+                            onChange={(e) =>
+                              setField("password", e.target.value)
+                            }
+                            isInvalid={!!errors.password}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                          </Form.Control.Feedback>
+                        </FloatingLabel>
+                      </Form.Group>
+                      <Form.Group>
+                        <div className="d-grid pt-1 mb-4">
+                          <Button variant="primary" size="lg" type="submit">
+                            Login
+                          </Button>
                         </div>
-                      </div>
-                      <div className="form-floating mb-4">
-                        <input
-                          type="password"
-                          className="form-control"
-                          id="floatingPassword"
-                          placeholder="Password123"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <label htmlFor="floatingPassword">Password</label>
-                      </div>
-                      <div className="pt-1 mb-4">
-                        <button
-                          className="btn btn-dark btn-lg btn-block"
-                          type="button"
-                          onClick={onLoginSubmit}
-                        >
-                          Login
-                        </button>
-                      </div>
-
+                      </Form.Group>
                       <a className="small text-muted" href="#!">
                         Forgot password?
                       </a>
-                    </form>
+                    </Form>
                   </div>
                 </div>
               </div>
--- frontend/flex-drone-frontend/src/pages/User/index.jsx
+++ frontend/flex-drone-frontend/src/pages/User/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { apis } from '../../utils/apis.js';
-import { getUserId } from '../../utils/processJwt.js';
+import React, { useState, useEffect } from "react";
+import { apis } from "../../utils/apis.js";
+import { getUserId } from "../../utils/processJwt.js";
+
 const UserProfile = () => {
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
@@ -20,13 +21,70 @@ const UserProfile = () => {
   }, []);
 
   return (
-    <div>
-      <p>Username: {username}</p>
-      <p>First name: {firstName}</p>
-      <p>Last name: {lastName}</p>
-      <p>Email: {email}</p>
-      <p>Role: {role}</p>
-    </div>
+    <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+      <div className="container py-5 h-100">
+        <div className="row justify-content-center align-items-center h-100">
+          <div className="col col-md-10 col-xl-8">
+            <div className="card" style={{ borderRadius: "15px" }}>
+              <div className="card-body p-4 text-center">
+                <div className="mt-3 mb-4">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                    className="rounded-circle img-fluid"
+                    style={{ width: "100px" }}
+                    alt="profile"
+                  />
+                </div>
+                <h4 className="card-title mb-4">{`${firstName} ${lastName}`}</h4>
+                <h6 className="card-subtitle mb-2">{role}</h6>
+                <h6 className="card-subtitle text-muted mb-4">{`@${username}`}</h6>
+                <p className="card-text text-muted mb-4">
+                  Email<span className="mx-2">|</span> {email}
+                </p>
+
+                <div className="mb-4 pb-2">
+                  <btn
+                    className="btn btn-outline-primary btn-floating mx-1"
+                    type="button"
+                  >
+                    <i
+                      className="bi bi-microsoft-teams"
+                      style={{ color: "cornflowerblue" }}
+                    ></i>
+                  </btn>
+                  <btn
+                    className="btn btn-outline-primary btn-floating mx-1"
+                    type="button"
+                  >
+                    <i
+                      className="bi bi-twitter"
+                      style={{ color: "cornflowerblue" }}
+                    ></i>
+                  </btn>
+                  <btn
+                    className="btn btn-outline-primary btn-floating mx-1"
+                    type="button"
+                  >
+                    <i
+                      className="bi bi-linkedin"
+                      style={{ color: "cornflowerblue" }}
+                    ></i>
+                  </btn>
+                </div>
+                <btn
+                  className="btn btn-primary btn-rounded btn-lg mx-1"
+                  type="button"
+                >
+                  Message now
+                </btn>
+                <div className="d-flex justify-content-between text-center mt-5 mb-2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
+
 export default UserProfile;
